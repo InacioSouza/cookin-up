53aed58365d4ceb393a25fbf22fba9dd21a9f0ca
Inácio Souza Rocha <inaciosouzarocha69@gmail.com>
2026-05-14 14:39:52 -0300

    front - hotfix: Correção nas  Funções de obtenção de dados

--- src/http/index.ts
+++ src/http/index.ts
@@ -7,13 +7,13 @@ async function obterDadosURL<T>(url: string) {
 }
 
 export async function obterCategorias() {
-    return obterDadosURL<ICategoria>(
+    return obterDadosURL<ICategoria[]>(
         'https://gist.githubusercontent.com/InacioSouza/55338218095f3c70cb9f7c70beafc838/raw/4cff00f050a204ca3272b65ef227d0a0e2e77d41/ingredientes-projetinho-cooking-up.json'
     );
 }
 
 export async function obterReceitas() {
-    return obterDadosURL<IReceita>(
+    return obterDadosURL<IReceita[]>(
         'https://gist.githubusercontent.com/InacioSouza/6fa92cdc52d7c2c26eac469afc5a7fd2/raw/4cdf4393998c5c5de3d62883ad0ec64b1fb6cfb0/receitas-projetinho-cooking-up.json'
     );
 }
